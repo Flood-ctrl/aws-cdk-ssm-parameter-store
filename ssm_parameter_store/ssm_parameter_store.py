0e5be9003e2fcb4ed732e84759d36c1e3a505491
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-12-26 13:31:00 +0200

    Added json file

--- ssm_parameter_store/ssm_parameter_store.py
+++ ssm_parameter_store/ssm_parameter_store.py
@@ -1,3 +1,4 @@
+import json
 from aws_cdk import (
     core,
     aws_ssm as _ssm,
@@ -7,14 +8,28 @@ from aws_cdk import (
 class SsmParameterStoreStack(core.Stack):
 
     def __init__(self, scope: core.Stack, id: str,
-                ssm_parameters: dict,
                  **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        for parameter_name,string_value in ssm_parameters.items():
+        # for parameter_name,string_value in ssm_parameters.items():
 
-            ssm_parameter_store = _ssm.StringParameter(
-                self, f'{parameter_name}',
-                string_value=string_value,
-                parameter_name=parameter_name,
+        #     ssm_parameter_store = _ssm.StringParameter(
+        #         self, f'{parameter_name}',
+        #         string_value=string_value,
+        #         parameter_name=parameter_name,
+        #     )
+
+        
+        with open("ssm_parameters.json", 'rb') as f:
+            ssm_parameters = json.load(f)
+        
+        for key, value in ssm_parameters.items():
+            if 'lifecycle' in ssm_parameters:
+                key = '/' + ssm_parameters['lifecycle'] + key
+
+            _ssm.StringParameter(
+                self, 'SsmParameters',
+                string_value=value,
+                parameter_name=key
+
             )
